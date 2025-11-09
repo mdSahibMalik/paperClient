7dcbf5781a9c19323f362fdd53b3935eaa7af94f
ms malik <msmalik81929175@gmail.com>
2025-11-10 00:11:54 +0530

    add hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
+// this is another commentdf 
